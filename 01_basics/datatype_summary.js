28aa44d27c95892881b8bc2d0b782e9b0977c017
Deepak746 <62786118+Deepak746@users.noreply.github.com>
2024-05-28 15:27:23 +0000

    DataTypes Summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,31 @@
+// Is js a dynamically typed or statically typed language
+
+
+
+// Primitive Datatype (Call by Value)
+// 7 Types : String, Number, Boolean, null, undefined, symbol, BigInt 
+
+
+// Non Primitive (Call by Reference)
+
+// Arrays, Objects, Functions
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);

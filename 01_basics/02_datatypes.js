2283dc986109b7d257528ea0e2a8d6e8a515ac42
Deepak746 <62786118+Deepak746@users.noreply.github.com>
2024-05-28 14:30:44 +0000

    DataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+let name = "Deepak"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to the power 53
+// BigInt
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof(isLoggedIn))

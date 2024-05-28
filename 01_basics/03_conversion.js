8ae44b71a5b7d7355703ce92ed8da74fc534162e
Deepak746 <62786118+Deepak746@users.noreply.github.com>
2024-05-28 14:40:29 +0000

    conversion of DataTypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,34 @@
+let score = 33
+type_of_score = typeof(score)
+
+console.log(typeof score);
+console.log(type_of_score);
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber); //Returns Nan
+
+console.log(typeof valueInNumber); //Returns number
+
+// let score1 = '33abc'
+// type_of_score1 = typeof(score1)
+
+// console.log(typeof score1);
+// console.log(type_of_score1);
+
+// let valueInNumber1 = Number(score1)
+
+// console.log(valueInNumber); //Returns Nan
+
+// console.log(typeof valueInNumber); //Returns number
+
+/*
+"33" => 33
+"33abc" =>NaN 
+true =>1 ; false => 0
+"" => false
+"asdg" => true
+
+*/
+
+

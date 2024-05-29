874a5247ffead77c6533a30f00974fa559acddaf
Deepak746 <62786118+Deepak746@users.noreply.github.com>
2024-05-29 06:34:20 +0000

    Nums only

---
+++ 01_basics/06nums_and_maths.js
@@ -0,0 +1,11 @@
+const score = 400
+console.log(typeof(score),score);
+
+const balance = new Number(100)
+
+console.log(balance);
+
+console.log(balance.toString().length);  //[Number: 100]
+console.log(balance.toFixed(2));  // 100.00
+
+

a32d6ee154439508475a7adde79a014b2a2b1b65
Deepak746 <62786118+Deepak746@users.noreply.github.com>
2024-05-29 05:33:31 +0000

    Heap and stack

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -31,3 +31,24 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+let name1 = "deepakcodes"
+
+let name2 = name1  // takes only a copy
+
+name2 = "chaiaurcode"
+
+console.log(name1); //deepakcodes
+console.log(name2); //CHAIAURCODE
+
+let usr1 ={
+    email : "me@ggl.com",
+    upi : 'usr1@ybl'
+}
+
+let usr2 = usr1  // usr2 takes reference of usr1
+
+usr2.email = "someoneelse@ggl.com"
+
+console.log(usr1.email);  //someoneelse@ggl.com
+console.log(usr2.email);  //someoneelse@ggl.com
